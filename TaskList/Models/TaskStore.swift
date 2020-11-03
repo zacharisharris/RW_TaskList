056ea4cddffa4e8bc5aa66072f1fd2d713896577
zacharisharris <zacharisharris@gmail.com>
2020-11-03 21:15:48 +0200

    Added Tasks in TaskStore.

--- TaskList/Models/TaskStore.swift
+++ TaskList/Models/TaskStore.swift
@@ -6,5 +6,16 @@
 //
 
 class TaskStore {
+    var tasks = [
+        "Code a SwiftUI app",
+        "Make a new account",
+        "Visit a doctor",
+        "Make karaoke playlist",
+        "Shop groceries",
+        "Walk the cat",
+        "iOS Meetup",
+        "100 movie reboot marathon",
+        "Climb El Capitan"
+    ].map {Task(name: $0)}
     
 }
